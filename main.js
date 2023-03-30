5d444f560c5a4291f9cb30790b27b6de07b087e7
Marcin <binekmarcin@gmail.com>
2023-03-30 07:04:38 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+
+  autoUpdater.checkForUpdatesAndNotify();
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+autoUpdater.on('update-available', () => {
+  mainWindow.webContents.send('update_available');
+});
+
+autoUpdater.on('update-downloaded', () => {
+  mainWindow.webContents.send('update_downloaded');
+});
